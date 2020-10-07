f884df3a52cbc71130e22dc53dfeadc20f40d296
Matheus dos Santos Candiotti <matheuscandiotti@hotmail.com>
2020-10-07 18:44:54 -0300

    Commit Inicial

---
+++ pedidos/src/test/java/br/com/homeproject/pedidos/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.homeproject.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
