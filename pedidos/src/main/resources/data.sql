09edb3cf299a7e4f6fc1df7aa369be8e9d08d3de
Matheus dos Santos Candiotti <matheuscandiotti@hotmail.com>
2020-10-12 17:01:28 -0300

    Adiciona arquivo data.sql para inserts básicos

---
+++ pedidos/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO CLIENTE(nome) VALUES('Matheus');
+INSERT INTO CLIENTE(nome) VALUES('Bruno');
+INSERT INTO CLIENTE(nome) VALUES('Rita');
+
+INSERT INTO ENDERECO_ENTREGA(bairro, rua, numero, cidade, estado, cliente_id) VALUES('Bairro X', 'Rua X', 111, 'Cidade X', 'Estado X', 1);
+INSERT INTO ENDERECO_ENTREGA(bairro, rua, numero, cidade, estado, cliente_id) VALUES('Bairro Y', 'Rua Y', 222, 'Cidade Y', 'Estado Y', 2);
+INSERT INTO ENDERECO_ENTREGA(bairro, rua, numero, cidade, estado, cliente_id) VALUES('Bairro Z', 'Rua Z', 333, 'Cidade Z', 'Estado Z', 3);
+
+INSERT INTO PRODUTO(nome, valor) VALUES('Fogão', 599.00);
+INSERT INTO PRODUTO(nome, valor) VALUES('Geladeira', 799.00);
+INSERT INTO PRODUTO(nome, valor) VALUES('Máquina de Lavar', 399.00);
+INSERT INTO PRODUTO(nome, valor) VALUES('TV', 1299.00);
+INSERT INTO PRODUTO(nome, valor) VALUES('Rádio', 199.00);
